3e53c6cd7eb799972609b59dfed7d2540e539802
PAVITHRAA297 <23sc038@drngpit.ac.in>
2026-02-13 09:39:06 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,18 @@
+// Last updated: 2/13/2026, 9:39:06 AM
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
